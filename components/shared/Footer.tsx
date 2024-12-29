a2ef1461c2f1cd4ba216535fd2e80fa99a5eae5c
Sayo Adegoroye <steve.legion94@gmail.com>
2024-12-29 19:22:47 +0000

    eslint restrictions

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
 
 import { Typography } from "@/components/ui/typography";
--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable @typescript-eslint/no-unused-expressions */
 "use client";
 
 import React, { useState, useEffect } from "react";
